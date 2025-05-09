ae96ebdf112962c52f1a51592e09e0e230cd47f2
Jack700103 <wqzl0852@leeds.ac.uk>
2025-05-09 11:48:14 +0000

    data

--- AbsoluteValue.asm
+++ AbsoluteValue.asm
@@ -17,3 +17,20 @@ M=D
 M=0          
 
 (END)
+
+
+
+//@R0: Load the address of register R0 into register A.
+//D=M: Give the value of A (R0) to D.
+//@POSITIVE D; JGE: If D ≥ 0, jump to POSITIVE.
+//If x<0:
+//@R1: Load the address of register R1 into register A.
+//M=- D: Send the negative value of the D register to the memory (R1) indicated by A.
+//@R2: Load the address of register R2 into register A.
+//M=1: Write 1 to the address indicated by register A, which is the unit indicated by R2. Here, R2 is used to indicate that x is negative.
+//@END and 0; JMP: Unconditionally jump to END tag.
+//If x ≥ 0 (i.e. jumps to POSITIVE label):
+//@R1: Load the address of register R1 into register A.
+//M=D: Move the number in the D register into the memory (R1) at the address specified by the A register.
+//@R2: Load the address of register R2 into register A.
+//M=0: Write 0 to the address pointed to by the A register, which is the address pointed to by R2. The meaning is that x is a non negative number.
--- Xor.asm
+++ Xor.asm
@@ -6,3 +6,10 @@ D=D^M
 @R2
 M=D           
 
+//@R0: Load the address of register R0 into register A.
+//D=M: Load the value of the memory unit (i.e. R0) pointed to by register A into register D.
+//@R1: Load the address of register R1 into register A.
+//D = D ^ M: XOR the contents of the D register with the contents of the memory cell addressed by A (R1) and return it to D.
+//@R2: Load the address of register R2 into register A.
+//M=D: Store the value in the D register in the memory location pointed to by the A register (i.e. R2).
+
